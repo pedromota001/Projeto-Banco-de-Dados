837bc7d0cbd2af2e791dacdac783828fa8760174
Pedro Mota <pedromotalindoso@gmail.com>
2024-10-31 18:28:52 -0300

    criacao de outras tabelas do modelo fisico

--- projetoBanco.py
+++ projetoBanco.py
@@ -15,15 +15,38 @@ try:
         cursor.execute("USE webdriver_db")
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS planos(
-            id BIGINT AUTO_INCREMENT,
+            id INT AUTO_INCREMENT,
             nome VARCHAR(20) NOT NULL,
             duracao FLOAT,
             data_aquisicao VARCHAR(20),
             espaco_por_usuario FLOAT, 
             PRIMARY KEY(id) 
-        )
+        );
         """)
-        print("Tabela criada com sucesso")
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS instituicoes(
+            id_instituicao INT AUTO_INCREMENT,
+            nome VARCHAR(20) NOT NULL,
+            endereco VARCHAR(30) NOT NULL,
+            causa_social VARCHAR(15),
+            id_plano INT,
+            PRIMARY KEY(id_instituicao),
+            FOREIGN KEY(id_plano) REFERENCES planos(id)
+        );
+        """)
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios(
+            id_usuario INT AUTO_INCREMENT,
+            email VARCHAR(20) NOT NULL,
+            senha VARCHAR(20) NOT NULL, 
+            login VARCHAR(15) NOT NULL,
+            data_ingresso DATE,
+            id_instituicao INT, 
+            PRIMARY KEY(id_usuario),
+            FOREIGN KEY(id_instituicao) REFERENCES instituicoes(id_instituicao)
+        );
+        """)
+        print("Tabelas criadas com sucesso")
 except mysql.connector.Error as erro:
     print(f"conexao mal sucedida: {erro}")
 finally:
