387ce05353bc813bb9c0152099a1eab4893d6072
Gustavo Porto <Gustavolima0203@gmail.com>
2024-11-06 18:16:32 -0300

    Criação da função procudere de contar usuarios para a tabela de arquivos

--- procedures.py
+++ procedures.py
@@ -16,4 +16,26 @@ def verificar_atividades():
     conexao.close()
 verificar_atividades()
 
+def conta_usuario(id_arquivo):
+    cursor = conexao.cursor()
+
+    try:
+        verifica_arquivo_query = '''
+        SELECT COUNT(*) 
+        FROM arquivos
+        WHERE id_arquivo = %s;
+        '''
+        cursor.execute(verifica_arquivo_query, (id_arquivo,))
+        resultado = cursor.fetchone()
+        return result[0] if resul else 0
+    except mysql.connector.Error as erro:
+        print("Erro ao acessar o banco de dados", erro)
+        return None
+    finally:
+        cursor.close()
+        conexao.close()
+id_arquivo = 1
+print(f"Usuarios  distintos  com acesso ao arquivo {id_arquivo}: {conta_usuario(id_arquivo)}")
+
+
 
