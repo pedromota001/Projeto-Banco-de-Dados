345151e30866e25c207b9efcdae523f6a0142f7e
Rondon Guilherme <128708099+rondsszz@users.noreply.github.com>
2024-11-06 09:20:44 -0300

    atualização da criação das roles

--- CriacaoDeRoles.py
+++ CriacaoDeRoles.py
@@ -1,50 +1,47 @@
 import mysql.connector
 from mysql.connector import Error
 
-
 def cria_role_PapelUsuario(conexao, id_usuario):
-     try:
+    try:
         cursor = conexao.cursor()
-        cursor.execute("""
-        CREATE ROLE PapelUsuario,
-        GRANT CONNECT TO PapelUsuario,
-        GRANT SELECT ON arquivos_usuario TO PapelUsuario,
-        GRANT INSERT ON arquivos_usuario TO PapelUsuario,
-        GRANT UPDATE ON arquivos_usuario TO PapelUsuario
-        """, 
+        cursor.execute("CREATE ROLE PapelUsuario")
+        cursor.execute("GRANT CONNECT ON DATABASE nome_do_banco TO PapelUsuario")
+        cursor.execute("GRANT SELECT ON arquivos_usuario TO PapelUsuario")
+        cursor.execute("GRANT INSERT ON arquivos_usuario TO PapelUsuario")
+        cursor.execute("GRANT UPDATE ON arquivos_usuario TO PapelUsuario")
         conexao.commit()
-    except Error as erro:
-        print(f"Nao foi possivel criar role: {erro}")
-        return None
+        print("Role 'PapelUsuario' criada com sucesso.")
+    except Exception as erro:  
+        conexao.rollback()  
+        print(f"Não foi possível criar a role: {erro}")    
     finally:
         cursor.close()
+
 
 def cria_role_PapelEmpresa(conexao):
-     try:
+    try:
         cursor = conexao.cursor()
-        cursor.execute("""
-        CREATE ROLE PapelEmpresa,
-        GRANT CONNECT TO PapelEmpresa,
-        GRANT SELECT ON view_empresa TO PapelEmpresa,
-        GRANT SELECT ON view_empresa TO PapelEmpresa
-        """,
+        cursor.execute("CREATE ROLE PapelEmpresa")
+        cursor.execute("GRANT CONNECT ON DATABASE nome_do_banco TO PapelEmpresa")
+        cursor.execute("GRANT SELECT ON view_empresa TO PapelEmpresa")
         conexao.commit()
-    except Error as erro:
-        print(f"Nao foi possivel criar role: {erro}")
-        return None
+        print("Role 'PapelEmpresa' criada com sucesso.")
+    except Exception as erro:
+        conexao.rollback()
+        print(f"Não foi possível criar a role: {erro}")
     finally:
         cursor.close()
 
+
 def cria_role_PapelAdm(conexao):
-     try:
+    try:
         cursor = conexao.cursor()
-        cursor.execute("""
-        CREATE ROLE PapelAdm,
-        GRANT ALL PRIVILEGES ON webdriver_db.* TO 'PapelAdm';
-        """,
+        cursor.execute("CREATE ROLE PapelAdm")
+        cursor.execute("GRANT ALL PRIVILEGES ON DATABASE nome_do_banco TO PapelAdm")
         conexao.commit()
-    except Error as erro:
-        print(f"Nao foi possivel criar role: {erro}")
-        return None
+        print("Role 'PapelAdm' criada com sucesso.")    
+    except Exception as erro:
+        conexao.rollback()
+        print(f"Não foi possível criar a role: {erro}")    
     finally:
         cursor.close()
