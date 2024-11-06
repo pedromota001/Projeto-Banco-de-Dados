b7f83e39d290b5884dadbeced4ca980e6996ea90
Rondon Guilherme <128708099+rondsszz@users.noreply.github.com>
2024-11-06 09:38:02 -0300

    Update CriacaoDeRoles.py

--- CriacaoDeRoles.py
+++ CriacaoDeRoles.py
@@ -5,7 +5,7 @@ def cria_role_PapelUsuario(conexao, id_usuario):
     try:
         cursor = conexao.cursor()
         cursor.execute("CREATE ROLE PapelUsuario")
-        cursor.execute("GRANT CONNECT ON DATABASE nome_do_banco TO PapelUsuario")
+        cursor.execute("GRANT CONNECT ON DATABASE webdriver_db TO PapelUsuario")
         cursor.execute("GRANT SELECT ON arquivos_usuario TO PapelUsuario")
         cursor.execute("GRANT INSERT ON arquivos_usuario TO PapelUsuario")
         cursor.execute("GRANT UPDATE ON arquivos_usuario TO PapelUsuario")
@@ -22,7 +22,7 @@ def cria_role_PapelEmpresa(conexao):
     try:
         cursor = conexao.cursor()
         cursor.execute("CREATE ROLE PapelEmpresa")
-        cursor.execute("GRANT CONNECT ON DATABASE nome_do_banco TO PapelEmpresa")
+        cursor.execute("GRANT CONNECT ON DATABASE webdriver_db TO PapelEmpresa")
         cursor.execute("GRANT SELECT ON view_empresa TO PapelEmpresa")
         conexao.commit()
         print("Role 'PapelEmpresa' criada com sucesso.")
@@ -37,7 +37,7 @@ def cria_role_PapelAdm(conexao):
     try:
         cursor = conexao.cursor()
         cursor.execute("CREATE ROLE PapelAdm")
-        cursor.execute("GRANT ALL PRIVILEGES ON DATABASE nome_do_banco TO PapelAdm")
+        cursor.execute("GRANT ALL PRIVILEGES ON DATABASE webdriver_db TO PapelAdm")
         conexao.commit()
         print("Role 'PapelAdm' criada com sucesso.")    
     except Exception as erro:
