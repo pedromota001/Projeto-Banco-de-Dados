00b9dcbac614f59c27c40f48eed1094ac11bb534
Guga2111 <luisgosampaio@gmail.com>
2024-11-06 10:25:17 -0300

    criacao de view do historico de operacoes

--- CriacaoDeViews.py
+++ CriacaoDeViews.py
@@ -18,3 +18,25 @@ def view_arquivo_usuario(conexao):
         return None
     finally:
         cursor.close()
+
+
+
+def view_historico_usuario(conexao):
+    try:
+        cursor = conexao.cursor()
+        cursor.execute("""
+        CREATE OR REPLACE VIEW historico_usuario AS
+        SELECT
+        o.data_op,
+        o.hora_op,
+        o.tipo_operecao
+        FROM operacoes o
+        INNER JOIN usuarios u ON o.id_usuario = u.id_usuario
+        WHERE o.id_usuario = @id_usuario_logado
+        GRANT SELECT ON historico_operacoes_usuario TO usuario_logado;                                                                                                                                       
+        """)
+    except Error as erro:
+        print(f"Nao foi possivel criar view: {erro}")
+        return None
+    finally:
+        cursor.close()
