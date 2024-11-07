0826f773a9fd6e362735a141e64d330a24f214df
Guga2111 <luisgosampaio@gmail.com>
2024-11-07 18:47:27 -0300

    ajeitando as duas ultimas procedures que faltou na terceira: faltou fazer vice-versa; e na ultima setei o atributo incorreto.

--- procedures.py
+++ procedures.py
@@ -1,76 +1,103 @@
-import mysql.connector
-from mysql.connector import Error
-
-import mysql.connector
-
+import mysql.conncetor
+from datetime import date
 
 def verificar_atividades(conexao):
-    try:
-        cursor = conexao.cursor()
-        query = """
-        CREATE PROCEDURE verificar_atividades()
-        BEGIN
-            UPDATE atividades_recentes SET ultima_versao = CURDATE();
-            SELECT CONCAT(ROW_COUNT(), ' linhas foram atualizadas com a data atual.') AS mensagem_sucesso;
-        END;
-        """
-        cursor.execute(query)
+    cursor = conexao.cursor()
+    cursor.execute("""
+    
+
+CREATE PROCEDURE verificar_atividades()
+BEGIN
+    SET @data_atual = CURDATE();
+    UPDATE atividades_recentes SET ultima_versao = @data_atual;
+    SELECT CONCAT(ROW_COUNT(), ' linhas foram atualizadas com a data atual.') AS mensagem_sucesso;
+END; 
+
+     """)
+    data_atual = date.today()
+    query = "UPDATE atividades_recentes SET ultima_versao = %s"
+    cursor.execute(query, (data_atual,))
+    if cursor.rowcount > 0:
         conexao.commit()
-        print("Procedure criada com sucesso!")
-    except mysql.connector.Error as err:
-        print(f"Erro ao criar a procedure: {err}")
-    finally:
-        cursor.close()
+        print('Tabela Atividades atualizadas com sucesso!')
+    else:
+        conexao.rollback()
+        print('Nenhuma linha foi atualizada com sucesso!')
+    cursor.close()
+    conexao.close()
 
-def conta_usuario(conexao):
-
-    try:
-        cursor = conexao.cursor()
-        cursor.execute("""   
-        CREATE PROCEDURE ContaUsuarios(IN arquivo_id INT, OUT total_usuarios INT)
-        BEGIN
-            SELECT COUNT(DISTINCT id_usuario) + COUNT(DISTINCT id_usuario_compartilhado) INTO total_usuarios
-            FROM arquivos a 
-            INNER JOIN compartilhamentos c ON c.id_arquivo = a.id_arquivo
-            WHERE a.id_arquivo = arquivo_id;
-            IF total_usuarios IS NULL THEN
-                SET total_usuarios = 0;
-            END IF;
-        END; 
+def conta_usuario(conexao, id_arquivo):
+    cursor = conexao.cursor()
+    cursor.execute("""
+       
+    CREATE PROCEDURE ContaUsuarios(IN arquivo_id INT, OUT total_usuarios INT)
+    BEGIN
+        SELECT COUNT(DISTINCT id_usuario) INTO total_usuarios
+        FROM arquivos
+        WHERE id_arquivo = arquivo_id;
+        IF total_usuarios IS NULL THEN
+            SET total_usuarios = 0;
+        END IF;
+    END; 
+    
         """)
-        conexao.commit()
-    except Error as erro:
-        print(f"Erro ao criar procedure de contagem de usuarios: {erro}")
+    try:
+        verifica_arquivo_query = '''
+        SELECT COUNT(*) 
+        FROM arquivos
+        WHERE id_arquivo = %s;
+        '''
+        cursor.execute(verifica_arquivo_query, (id_arquivo,))
+        resultado = cursor.fetchone()
+        return result[0] if resul else 0
+    except mysql.connector.Error as erro:
+        print("Erro ao acessar o banco de dados", erro)
         return None
     finally:
         cursor.close()
+        conexao.close()
+id_arquivo = 1
+print(f"Usuarios  distintos  com acesso ao arquivo {id_arquivo}: {conta_usuario(id_arquivo)}")
 
-def chavear_arquivo(conexao):
+def chavear_arquivo(conexao, id_arquivo):
     cursor = conexao.cursor()
 
     try:
         cursor.execute("""
-            CREATE PROCEDURE chavear_arquivo(IN p_id_arquivo INT)
-            BEGIN
-                IF EXISTS (
-                    SELECT 1
-                    FROM arquivos
-                    WHERE id_arquivo = p_id_arquivo
-                ) AND EXISTS (
-                    SELECT 1
-                    FROM atividades_recentes
-                    WHERE id_arquivo = p_id_arquivo AND acesso = 'p'
-                ) THEN
-                    UPDATE atividades_recentes
-                    SET acesso = 'np'
-                    WHERE id_arquivo = p_id_arquivo;
-                ELSE
-                    SIGNAL SQLSTATE '45000'
-                    SET MESSAGE_TEXT = 'O arquivo não existe ou não está marcado como prioritário.';
-                END IF;
-            END;
-        """)
-        conexao.commit()
+    CREATE PROCEDURE chavear_arquivo(IN p_id_arquivo INT)
+    BEGIN
+        IF EXISTS (
+            SELECT 1
+            FROM arquivos
+            WHERE id_arquivo = p_id_arquivo
+        ) THEN
+            IF EXISTS (
+                SELECT 1
+                FROM atividades_recentes
+                WHERE id_arquivo = p_id_arquivo AND acesso = 'p'
+            ) THEN
+                UPDATE atividades_recentes
+                SET acesso = 'np'
+                WHERE id_arquivo = p_id_arquivo;
+            ELSEIF EXISTS (
+                SELECT 1
+                FROM atividades_recentes
+                WHERE id_arquivo = p_id_arquivo AND acesso = 'np'
+            ) THEN
+                UPDATE atividades_recentes
+                SET acesso = 'p'
+                WHERE id_arquivo = p_id_arquivo;
+            ELSE
+                SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'O arquivo não existe ou não está marcado como prioritário ou não prioritário.';
+            END IF;
+        ELSE
+            SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'O arquivo não existe na tabela arquivos.';
+        END IF;
+    END;
+""")
+
         print("Procedimento 'chavear_usuario' criado com sucesso.")
 
     except Error as erro:
@@ -78,9 +105,10 @@ def chavear_arquivo(conexao):
     finally:
         cursor.close()
 
-def remover_acessos(conexao):
+def remover_acessos(conexao, id_arquivo):
+    cursor = conexao.cursor()
+
     try:
-        cursor = conexao.cursor()
         cursor.execute("""
 
             CREATE PROCEDURE remover_acessos(IN r_id_arquivo INT)
@@ -95,15 +123,16 @@ def remover_acessos(conexao):
                     FROM compartilhamentos
                     WHERE id_arquivo = r_id_arquivo;   
 
-                    UPDATE arquivos
-                    SET permissao = false
+                    UPDATE compartilhamentos
+                    SET id_usuario_compartilhado = NULL
                     WHERE id_arquivo = r_id_arquivo
                     AND id_usuario != v_id_usuario_dono;
                 END IF;
             END;                  
         """)
-        conexao.commit()
+
         print("Procedimento 'remover_acessos' criado com sucesso.")
+        conexao.commit()
     except Exception as error:
         print(f"Erro ao remover acessos: {error}")
     finally:
