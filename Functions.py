bc016ac3d2cc536ee1b5694dd967369c220b18cc
Rondon Guilherme <128708099+rondsszz@users.noreply.github.com>
2024-11-08 10:44:46 -0300

    criação da function que verifica arquivos antigos

---
+++ Functions.py
@@ -0,0 +1,25 @@
+def Function(conexao):
+    try:
+        cursor = conexao.cursor()
+        query = """
+        CREATE FUNCTION verificar_arquivo_antigo(data_ult_modificacao DATE)
+        RETURNS BOOLEAN
+        DETERMINISTIC
+        BEGIN
+            DECLARE resultado BOOLEAN
+            IF DATEDIFF(CURDATE(), data_ult_modificacao) > 100 THEN
+                SET resultado = TRUE;
+            ELSE
+                SET resultado = FALSE;
+            END IF;
+            
+            RETURN resultado;
+        END;
+        """
+        cursor.execute(query)
+        conexao.commit()
+        print("Função 'verificar_arquivo_antigo' criada com sucesso!")
+    except mysql.connector.Error as err:
+        print(f"Erro ao criar a função: {err}")
+    finally:
+        cursor.close()
